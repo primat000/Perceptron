0a91000f0e12cb7acfd808f9b57c24631f29623a
Olga <ek.stepano@yandex.ru>
2015-09-28 22:57:49 +0300

    First send

--- Net_lib/main.cpp
+++ Net_lib/main.cpp
@@ -12,17 +12,10 @@ int main()
     vector<int> a(m, m + sizeof(m) / sizeof(int) );
     vector<double> b;
     b.push_back(1);
-
     double (*f)(double) = functions::th;
-    //layer b();
     neuron n(logistic_function);
     neuron_net net(3,a);
     net.info();
-
     net.prediction(b);
-
-    /*cout<<th(1)<<endl;
-    cout<<th(2)<<endl;*/
-
     return 0;
 }
